e2c0e3f8ab25d9ba3dc68bded77c7febf023220a
ChristopherLMiller <chris@christopherleemiller.me>
2020-12-02 10:16:55 -0500

    only run if command prefix used

--- index.js
+++ index.js
@@ -15,44 +15,47 @@ bot.on('ready', () => {
 
 // now process messages
 bot.on('message', msg => {
-    const [command, params] = msg.content.split(' ');
+    if (msg.content.startsWith('!')) {
+
+        const [command, params] = msg.content.split(' ');
 
 
-    switch (command) {
-        case 'roll':
-        case '!roll': {
-            // first check that they supplied the params, needed
-            if (params) {
-                msg.channel.send(`${msg.author.username} rolled some dice`);
+        switch (command) {
+            case 'roll':
+            case '!roll': {
+                // first check that they supplied the params, needed
+                if (params) {
+                    msg.channel.send(`${msg.author.username} rolled some dice`);
 
-                // 1) step is to see if they want to roll multiple dice. they will be separated by a comma, if no comma found then singular roll
-                const dice = params.includes(',') ? params.split(',') : [params];
+                    // 1) step is to see if they want to roll multiple dice. they will be separated by a comma, if no comma found then singular roll
+                    const dice = params.includes(',') ? params.split(',') : [params];
 
-                // 2) Iterate over the array rolling the appropriate dice
-                dice.forEach(die => {
-                    // verify that the die is of correct format
-                    if (die.toLowerCase().includes('d')) {
-                        const [count, sides] = die.split('d');
+                    // 2) Iterate over the array rolling the appropriate dice
+                    dice.forEach(die => {
+                        // verify that the die is of correct format
+                        if (die.toLowerCase().includes('d')) {
+                            const [count, sides] = die.split('d');
 
-                        // lets roll the die now
-                        let dieRolls = [];
+                            // lets roll the die now
+                            let dieRolls = [];
 
-                        for (let index = 1; index <= count; index++) {
-                            const result = Math.floor(Math.random() * sides) + 1;
-                            dieRolls[index-1] = result;
-                            
+                            for (let index = 1; index <= count; index++) {
+                                const result = Math.floor(Math.random() * sides) + 1;
+                                dieRolls[index-1] = result;
+                                
+                            }
+                            msg.reply(`Roll for ${die}: ${dieRolls.join(', ')}`);
+                        } else {
+                            // the die was invalid let the user know
+                            msg.reply(`The roll of ${die} was invalid, format is (x)d(y), where (x) is the number of die and (y) is how many sides it has`);
                         }
-                        msg.reply(`Roll for ${die}: ${dieRolls.join(', ')}`);
-                    } else {
-                        // the die was invalid let the user know
-                        msg.reply(`The roll of ${die} was invalid, format is (x)d(y), where (x) is the number of die and (y) is how many sides it has`);
-                    }
-                });
+                    });
 
 
-            } else {
-                // didn't suppy the params
-                msg.reply('You must tell me how many of what dice to roll!');
+                } else {
+                    // didn't suppy the params
+                    msg.reply('You must tell me how many of what dice to roll!');
+                }
             }
         }
     }
